18d340860222330e6a28b8078aaa1471a8f2743b
David Martinez <davestringm@gmail.com>
2023-04-02 19:42:46 -0600

    chore: setup api server

---
+++ app/settings.py
@@ -0,0 +1,1 @@
+"""Flask settings module."""
--- manage.py
+++ manage.py
@@ -1,1 +1,10 @@
 """Project's command-line utility module."""
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+if __name__ == "__main__":
+    app.run()
